54ee2356b081a664059d38da78a76a8c26c30e65
Douglas Shinzato <shinzato.douglas@gmail.com>
2025-02-17 20:05:52 -0400

    cors origin adjustment for railway deploy

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ import productRoutes from './routes/productRoutes'
 export const app = fastify()
 
 app.register(cors, {
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 })
 
-app.get("/", async () => {
-  return { message: "Hello!" };
-});
+app.get('/', async () => {
+  return { message: 'Hello!' }
+})
 
 app.register(productRoutes, { prefix: '/products' })
