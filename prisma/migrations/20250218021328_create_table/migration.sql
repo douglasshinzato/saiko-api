2a8455d90d28667737c6010053f474d9b14cd88d
Douglas Shinzato <shinzato.douglas@gmail.com>
2025-02-17 22:19:36 -0400

    migrations adjustments

--- prisma/migrations/20250218021328_create_table/migration.sql
+++ prisma/migrations/20250218021328_create_table/migration.sql
@@ -1,13 +1,12 @@
 -- CreateTable
 CREATE TABLE "Products" (
     "id" TEXT NOT NULL,
-    "barcode" TEXT NOT NULL,
+    "barcode" TEXT,
     "brand" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "category" TEXT NOT NULL,
     "description" TEXT,
     "price" DECIMAL(65,30) NOT NULL,
-    "stock" DOUBLE PRECISION,
     "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updateAt" TIMESTAMP(3) NOT NULL,
 
